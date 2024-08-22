91e41286dc32d09eb9b3fff9af356b56d9cafdce
Florian Hass <f91hama@gmail.com>
2024-08-22 13:59:34 +0000

    Create about page

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse('<h1 style="color: red">Index Page</h1>')
+
+def about(requst):
+    return HttpResponse('<h1 style="color: blue">About Page</h1>')
