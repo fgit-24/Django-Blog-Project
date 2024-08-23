6be413cfcb405ef305ba4acf821b6aed4040ee4b
Florian Hass <f91hama@gmail.com>
2024-08-23 13:53:35 +0000

    Add cards to index.html

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('', views.index, name='blog-index'),
-    path('about/', views.about, name='blog-about')
+    path('index/', views.index, name='index-index'),
+    path('about/', views.about, name='blog-index'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1 style="color: red">Index Page</h1>')
+    return render(request, 'blog/index.html')
 
-def about(requst):
-    return HttpResponse('<h1 style="color: blue">About Page</h1>')
+def about(request):
+    return render(request, 'blog/about.html')
