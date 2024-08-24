847a71093e9e6b9996a1484144b5be3ea29de1f1
Florian Hass <f91hama@gmail.com>
2024-08-24 16:50:29 +0000

    Create forms.py with class signupform

--- users/views.py
+++ users/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
 
 # Create your views here.
 def sign_up(request):
-    return render(request, 'users/sign_up.html')
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index-index')
+    else:
+        form = SignUpForm
+    context = {
+        'form': form,
+    }
+    return render(request, 'users/sign_up.html', context)
