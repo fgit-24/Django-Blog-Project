748d6fc3ba927e78b7412f36303b6cd2d77e48d4
Florian Hass <f91hama@gmail.com>
2024-08-24 15:11:23 +0000

    Set up users file structure

--- myproject/settings.py
+++ myproject/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
+    'users.apps.UsersConfig',
     "crispy_forms",
     "crispy_bootstrap4",
 ]
---
+++ users/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+
+# Create your views here.
+def sign_up(request):
+    return render(request, 'users/sign_up.html')
