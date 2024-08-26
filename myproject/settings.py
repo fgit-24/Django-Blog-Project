3cf27b0ca071bb877f0b5919dcd5b608c9a70449
Florian Hass <f91hama@gmail.com>
2024-08-26 08:44:35 +0000

    Change collectstatic folder

--- myproject/settings.py
+++ myproject/settings.py
@@ -135,10 +135,10 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = (BASE_DIR / 'media')
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+    BASE_DIR / 'static',
 ]
 
-STATIC_ROOT = (BASE_DIR / 'asset')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
