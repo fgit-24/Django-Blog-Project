24856b4f1f210c320ba892f4e51c5b35045965c3
Florian Hass <f91hama@gmail.com>
2024-09-03 16:01:51 +0000

    Change background image

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,5 +1,5 @@
 
-"""postscribers URL Configuration
+"""URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
