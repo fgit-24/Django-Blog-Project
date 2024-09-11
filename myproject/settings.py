b7afb9a8ce621e7b140bf1dec750aa8a831fb8ba
Florian Hass <f91hama@gmail.com>
2024-09-11 08:18:03 +0000

    Set debug to true

--- myproject/settings.py
+++ myproject/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', True)
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-fgit24-djangoblogprojec-2120s04d3ik.ws.codeinstitute-ide.net']
 
